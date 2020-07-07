01c67311d743ac1cbbcdee2b686aaf63fafff154
Elizabeth <elizabeth@ziemer.net>
2020-07-06 20:50:56 -0500

    badges, sigid, subcategories

--- tech/artpop.js
+++ tech/artpop.js
@@ -1,6 +1,6 @@
 var html = '<h3> contents </h3>';
 
-var section1 = ['sigid', 'electronics', 'amateur', 'FRC']
+var section1 = ['sigid']
 for (i = 0; i<section1.length; i++){
 	html += '<div class="navlink"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
 }

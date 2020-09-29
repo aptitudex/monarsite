e8237dbd4a3dd9822f67b933669da71972bb6a48
Elizabeth <elizabeth@ziemer.net>
2020-09-29 12:01:18 -0500

    new blog post

--- blog/artpop.js
+++ blog/artpop.js
@@ -1,6 +1,6 @@
 var html = '<h3> contents </h3>';
 
-var section1 = ['7_3_2020', '7_5_2020', '8_25_2020', '8_28_2020']
+var section1 = ['7_3_2020', '7_5_2020', '8_25_2020', '8_28_2020', '9_29_2020']
 for (i = 0; i<section1.length; i++){
 	html += '<div class="navlink"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
 }
